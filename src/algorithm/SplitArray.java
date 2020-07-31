76b73efc884957dfa566201c7e17e3f04d0c8c08
ReminisX <1296753187@qq.com>
2020-07-31 19:42:50 +0800

    Create SplitArray.java

---
+++ src/algorithm/SplitArray.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+public class SplitArray {
+	public int[][] F;
+	
+	public int splitArray(int[] nums, int m) {
+		int result = 0;
+		int[][] F = new int[nums.length][nums.length];
+		
+		for(int i = 0; i < nums.length; i++) {
+			for(int j = 0; j < i; j++) {
+				
+			}
+		}
+		
+		
+		return result;
+    }
+}
