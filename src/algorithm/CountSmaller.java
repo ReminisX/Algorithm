a6d0a5406dc519ec8a637a69f88e47617001c7dc
ReminisX <1296753187@qq.com>
2020-07-31 19:42:45 +0800

    Update CountSmaller.java

--- src/algorithm/CountSmaller.java
+++ src/algorithm/CountSmaller.java
@@ -1,10 +1,15 @@
 package algorithm;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class CountSmaller {
-    public List<Integer> countSmaller(int[] nums) {
+	
+	/*
+	 * function 1: simply solution
+	 */
+    public List<Integer> countSmaller1(int[] nums) {
     	int len = nums.length;
     	List<Integer> counts = new ArrayList<Integer>();
     	
@@ -14,9 +19,40 @@ public class CountSmaller {
     			if(nums[i] > nums[j]) {
     				k++;
     			}
-    			counts.add(k);
     		}
+    		counts.add(k);
     	}
+		return counts;
+    }
+    
+	/*
+	 * function 2: insert sort solution
+	 */
+    public List<Integer> countSmaller2(int[] nums){
+    	List<Integer> counts = new ArrayList<Integer>();  //初始化结果表
+    	if(nums.length == 0) {
+    		return counts;
+    	}
+    	counts.add(0);
+    	int k, temp;
+    	int count = 0;
+    	for(int i=nums.length-2; i>=0; i--) {
+    		k = i;
+    		while(k < nums.length-1) {
+    			if(nums[k] > nums[k+1]) {
+    				temp = nums[k+1];
+    				nums[k+1] = nums[k];
+    				nums[k] = temp;
+    				k++;
+    				count++;
+    			}else {
+    				break;
+    			}
+    		}
+    		counts.add(count);
+    		count = 0;
+    	}
+    	Collections.reverse(counts);
 		return counts;
     }
     
