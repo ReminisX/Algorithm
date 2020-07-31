ed3ea538cceb7756a9727a1f8b8ed4c0abce0dfa
ReminisX <1296753187@qq.com>
2020-07-31 19:42:54 +0800

    Create Test.java

---
+++ src/algorithm/Test.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+	
+	
+
+	public static void main(String[] args) {
+		String str = "sdfghjk";
+		char[] char1 = str.toCharArray();
+		System.out.println(char1[0] == 115);
+	}
+
+}
